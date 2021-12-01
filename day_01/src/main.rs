21107ee5a98237bbcd55994c015ff127af7940e5
Andre Bossard <anbossar@microsoft.com>
2021-12-01 23:46:21 +0100

    task 2 done

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -5,22 +5,29 @@ fn main() {
     let filename = "input.txt";
     let content = fs::read_to_string(filename).expect("Something went wrong reading the file");
     // split by linebreaks
-    let lines: Vec<&str> = content.split("\n").collect();
+    let lines = content.split("\n");
+    // convert itertor to vector
+    let nums = lines.filter(|s| s.len() > 0).map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    // enumerate lines
     // iterate over lines
     let mut count = 0;
     let mut prev = 99999;
-    for line in lines {
+    // convert string vector to int vector
+    // enumerate nums
+    for (i, num) in nums.iter().enumerate() {
         // check line not empty
-        if line.len() == 0 {
-            continue;
+        if i + 2 == nums.len() {
+            break;
         }
+
         // convert string to int
-        let line = line.parse::<i32>().unwrap();
-        if line > prev {
+        // sum list of ints
+        let sum = num + nums[i + 1] + nums[i + 2];
+        if sum > prev {
             count += 1;
         }
-        prev = line;
-        println!("{}", line);
+        prev = sum;
+        println!("{}", sum);
     }  
     println!("{}", count);  
 }
